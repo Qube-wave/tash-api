0ab07ccc370016629cfc9736b8cfe35685ba8a3c
Covenant Ogowale <justcovenant@outlook.com>
2026-07-06 16:32:16 +0100

    chore(auth): documentation for update payment settings dto

--- src/settings/dto/update-payment-settings.dto.ts
+++ src/settings/dto/update-payment-settings.dto.ts
@@ -1,53 +1,103 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsInt, IsObject, IsOptional, Min } from 'class-validator';
 
 export class UpdatePaymentSettingsDto {
+  @ApiProperty({
+    name: 'defaultCardId',
+    type: 'number',
+  })
   @IsOptional()
   @IsInt()
   defaultCardId?: number | null;
 
+  @ApiProperty({
+    name: 'defaultDirectDebitMandateId',
+    type: 'number',
+  })
   @IsOptional()
   @IsInt()
   defaultDirectDebitMandateId?: number | null;
 
+  @ApiProperty({
+    name: 'defaultWalletId',
+    type: 'number',
+  })
   @IsOptional()
   @IsInt()
   defaultWalletId?: number | null;
 
+  @ApiProperty({
+    name: 'requireTransactionPin',
+    type: 'boolean',
+  })
   @IsOptional()
   @IsBoolean()
   requireTransactionPin?: boolean;
 
+  @ApiProperty({
+    name: 'allowCardPayments',
+    type: 'boolean',
+  })
   @IsOptional()
   @IsBoolean()
   allowCardPayments?: boolean;
 
+  @ApiProperty({
+    name: 'allowDirectDebitPayments',
+    type: 'boolean',
+  })
   @IsOptional()
   @IsBoolean()
   allowDirectDebitPayments?: boolean;
 
+  @ApiProperty({
+    name: 'allowWalletPayments',
+    type: 'boolean',
+  })
   @IsOptional()
   @IsBoolean()
   allowWalletPayments?: boolean;
 
+  @ApiProperty({
+    name: 'allowMerchantPayments',
+    type: 'boolean',
+  })
   @IsOptional()
   @IsBoolean()
   allowMerchantPayments?: boolean;
 
+  @ApiProperty({
+    name: 'dailyTransferLimit',
+    type: 'number',
+  })
   @IsOptional()
   @IsInt()
   @Min(0)
   dailyTransferLimit?: number;
 
+  @ApiProperty({
+    name: 'dailyPaymentLimit',
+    type: 'number',
+  })
   @IsOptional()
   @IsInt()
   @Min(0)
   dailyPaymentLimit?: number;
 
+  @ApiProperty({
+    name: 'singleTransactionLimit',
+    type: 'number',
+  })
   @IsOptional()
   @IsInt()
   @Min(0)
   singleTransactionLimit?: number;
 
+  @ApiProperty({
+    name: 'notificationPreferences',
+    type: 'object',
+    additionalProperties: true,
+  })
   @IsOptional()
   @IsObject()
   notificationPreferences?: Record<string, unknown>;
