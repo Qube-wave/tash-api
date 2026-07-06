3767f10386d85e14fb5803b1dfe656544ff904c5
Covenant Ogowale <justcovenant@outlook.com>
2026-07-06 16:33:36 +0100

    chore(auth): documentation for transaction pin dto

--- src/settings/dto/transaction-pin.dto.ts
+++ src/settings/dto/transaction-pin.dto.ts
@@ -1,17 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Length } from 'class-validator';
 
 export class CreateTransactionPinDto {
+  @ApiProperty({
+    name: 'pin',
+    type: 'string',
+  })
   @IsString()
-  @Length(4, 6)
+  @Length(4, 4)
   pin!: string;
 }
 
 export class UpdateTransactionPinDto {
+  @ApiProperty({
+    name: 'currentPin',
+    type: 'string',
+  })
   @IsString()
-  @Length(4, 6)
+  @Length(4, 4)
   currentPin!: string;
 
+  @ApiProperty({
+    name: 'newPins',
+    type: 'string',
+  })
   @IsString()
-  @Length(4, 6)
+  @Length(4, 4)
   newPin!: string;
 }
